31fcf65761254defb35433574a1809384a607da0
tariktabakovic <tariktab97@gmail.com>
2020-02-11 11:59:34 -0500

    creates store variable with createStore()

--- src/index.js
+++ src/index.js
@@ -67,6 +67,10 @@ const rootReducer = combineReducers (
     }
 )
 
+const store = createStore (
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 
