311fa8256913a36f5703f82e8015102e46ec9f37
tariktabakovic <tariktab97@gmail.com>
2020-02-11 11:53:11 -0500

    creates actions for contact and jobs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {
+    createStore, 
+    combineReducers
+} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const UPDATE_CONTACT = 'UPDATE_CONTACT';
+function actionUpdateContact(contactName){
+    return {
+        type: UPDATE_CONTACT,
+        payload: {
+            contactName
+        }
+    }
+}
+
+
+const UPDATE_JOBS = 'UPDATE_JOBS';
+function actionUpdateJob(jobName){
+    return {
+        type: UPDATE_JOBS,
+        payload: {
+            jobName
+        }
+    }
+}
+
+
+
+
+
+const defaultContactSate= {
+    contact: ''
+}
+function contacts (state= defaultContactSate, action){
+    const newState= {...state};
+    switch(action.type){
+        case UPDATE_CONTACT:
+            newState.contact = action.payload.contactName;
+            break;
+        default:
+            break;
+    }
+    return newState;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
